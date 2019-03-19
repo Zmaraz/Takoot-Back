030a28724e40f213c7785b6ba70f802257e900f2
Kamaria DeRamus <krafixdesigns@gmail.com>
2019-03-19 19:57:04 -0400

    created QuizDAO

---
+++ src/main/java/com/revature/dao/QuizDAO.java
@@ -0,0 +1,29 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import com.revature.models.Quiz;
+
+public class QuizDAO implements DAO<Quiz>{
+
+	public List<Quiz> getAll() {
+		return null;
+	}
+
+	public Quiz getById(int id) {
+		return null;
+	}
+
+	public Quiz add(Quiz obj) {
+		return null;
+	}
+
+	public Quiz update(Quiz updatedObj) {
+		return null;
+	}
+
+	public boolean delete(int id) {
+		return false;
+	}
+
+}
