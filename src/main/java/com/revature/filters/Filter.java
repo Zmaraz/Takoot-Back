7f97cad1fd06c3c35a977d3c27b8fe17b7d14486
Mitchell-Elbus <elbus3@gmail.com>
2019-03-25 20:14:24 -0400

    dasda

--- src/main/java/com/revature/filters/Filter.java
+++ src/main/java/com/revature/filters/Filter.java
@@ -1,6 +1,8 @@
 package com.revature.filters;
 
 import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -21,8 +23,27 @@ public class Filter extends HttpFilter {
 
 	private static final long serialVersionUID = 1L;
 	
+	//private static Logger log = LogManager.getLogger(Filter.class);
+	
 	@Override
 	public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
+		System.out.println("Request intercepted by PreFilter");
+		setAccessControlHeaders(resp);
+		checkHeaderForToken(req);
+//		checkCookieForToken(req);
+		chain.doFilter(req, resp);
+	}
+	
+	private void setAccessControlHeaders(HttpServletResponse resp) {
+		System.out.println("Attaching CORS headers to HTTP response");
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+		resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+		resp.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization");
+		resp.setHeader("Access-Control-Expose-Headers", "Authorization");
+	}
+	
+	
+	private void checkHeaderForToken(HttpServletRequest req) {
 		System.out.println("Inside of JwtAuthFilter.doFilter()");
 		
 		// 1. Get the HTTP header named "Authorization"
@@ -33,7 +54,9 @@ public class Filter extends HttpFilter {
 			System.out.println("Request originates from an unauthenticated origin");
 			
 			// 2.1: If there is no header, or one that we provided, then go to the next step in the filter chain (target servlet)
-			chain.doFilter(req, resp);
+			System.out.println("lit lit lit:  " + req);
+			
+			
 			return;
 		}
 		// 3. Get the token
@@ -64,7 +87,6 @@ public class Filter extends HttpFilter {
 		}
 		
 		// 7. Send the request to the next filter in the chain (target servlet)
-		chain.doFilter(req, resp);
-	}
-	
+
+	}	
 }
