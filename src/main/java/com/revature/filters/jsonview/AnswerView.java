b37cfdd3e910b2118d72ce2dcc68c2fa54920af6
Ankit <ajpatel90@gmail.com>
2019-03-27 22:48:02 -0400

    fixed answer pojo

---
+++ src/main/java/com/revature/filters/jsonview/AnswerView.java
@@ -0,0 +1,9 @@
+package com.revature.filters.jsonview;
+
+import com.revature.filters.jsonview.QuestionView.Public;
+
+public class AnswerView {
+
+	public interface Public{}
+	public interface Private extends Public{}
+}
